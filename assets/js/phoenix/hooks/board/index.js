d74216bbe1711fd9c21b36b7900e6e4b0aeb9a8c
blakedietz <blakedietz@gmail.com>
2023-04-14 15:38:07 -0600

    simplifies a lot of use cases

--- assets/js/phoenix/hooks/board/index.js
+++ assets/js/phoenix/hooks/board/index.js
@@ -1,7 +1,7 @@
 const Board = {
   currentlyDragging: null,
   mounted() {
-    this.el.addEventListener('mouseup', (e) => {
+    this.el.addEventListener('click', (e) => {
       console.log('browser:board:mouseup');
       console.log('phx:board:user-clicked-board');
       this.pushEvent('user-clicked-board', { data: { y: e.offsetY, x: e.offsetX } });
