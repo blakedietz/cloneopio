b1de6a4f414e3aa5962da47b75d6f78571e1656e
blakedietz <blakedietz@gmail.com>
2023-04-17 15:10:23 -0600

    refactor: remove dead code

--- assets/js/phoenix/hooks/card/card.ts
+++ assets/js/phoenix/hooks/card/card.ts
@@ -8,82 +8,8 @@ import {
 export type PhoenixLiveViewPushEventHandler = (event: string, payload: object, onReply?: (reply: any, ref: any) => void) => void;
 
 export default class Card {
-  previousPosition: Position;
-
-  private mouseMoveTriggered: boolean = false;
-  private readonly pushEvent: PhoenixLiveViewPushEventHandler;
-  private readonly element: HTMLElement;
-
   constructor(element: HTMLElement, pushEvent: PhoenixLiveViewPushEventHandler) {
     this.pushEvent = pushEvent;
     this.element = element;
-    element.addEventListener('mousedown', this.handleDragStart);
   }
-
-  private handleDragStart = (event: MouseEvent): void => {
-    console.log('browser:card:mousedown');
-
-    document.addEventListener('mousemove', this.handleDrag);
-    document.addEventListener('mouseup', this.handleDragEnd);
-    // Stop any interaction for mouse events
-    const board: HTMLElement | null = document.querySelector('#board');
-    if (board) {
-      board.style.pointerEvents = 'none';
-    }
-
-    this.setPreviousPosition(event);
-  }
-
-  private handleDrag = (event: MouseEvent): void => {
-    console.log('browser:card:mousemove');
-
-    const newDisplacement = displacement({ x: this.previousPosition.x, y: this.previousPosition.y }, { x: event.clientX, y: event.clientY });
-
-    // Guard clause, no need to do anything if mouse movement hasn't happened.
-    if (!hasDisplacement(newDisplacement)) return;
-
-    this
-      .setElementDOMPosition(newDisplacement)
-      .setPreviousPosition(event);
-
-    this.mouseMoveTriggered = true;
-  };
-
-  private handleDragEnd = (_event: MouseEvent): void => {
-    console.log('browser:card:mouseup');
-
-    if (this.mouseMoveTriggered && this.element) {
-      console.log('phx:card:card-drag-end');
-      this.pushEvent('card-drag-end', { data: { y: this.element?.offsetTop, x: this.element?.offsetLeft, id: this?.element?.dataset?.cardId } });
-    }
-    else {
-      console.log('phx:card:card-clicked');
-      this.pushEvent('card-clicked', { data: { id: this?.element?.dataset?.cardId } });
-    }
-
-    document.removeEventListener('mousemove', this.handleDrag);
-    document.removeEventListener('mouseup', this.handleDragEnd);
-
-    const board: HTMLElement | null = document.querySelector('#board');
-    if (board) {
-      board.style.pointerEvents = 'auto';
-    }
-
-    this.mouseMoveTriggered = false;
-  };
-
-  private setPreviousPosition(event: MouseEvent): Card {
-    this.previousPosition = {
-      x: event.clientX,
-      y: event.clientY
-    };
-
-    return this;
-  };
-
-  private setElementDOMPosition({ δx, δy }: Displacement): Card {
-    this.element.style.left = `${this.element.offsetLeft - δx}px`;
-    this.element.style.top = `${this.element.offsetTop - δy}px`;
-    return this;
-  };
 }
